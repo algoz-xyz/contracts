dba3743f484d35be08bd3fa45877a93585ebc674
Nishant Aklecha <nishant.aklecha@gmail.com>
2022-05-18 11:26:47 +0530

    Fixes in Algoz contract

--- Algoz.sol
+++ Algoz.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract Algoz {
-    mapping(bytes32 => bool) public consumed_tokenId;
+    mapping(bytes32 => mapping(bytes32 => bool)) public consumed_tokenId;
     address public owner;
     bytes32 public merkle_root;
 
@@ -26,7 +26,7 @@ contract Algoz {
     // function used to validate if a captcha is valid
     function validate_captcha(bytes32 tokenId, bytes32[] calldata proof) public {
         require(MerkleProof.verify(proof, merkle_root, tokenId)); // verify merkle proof of tokenId
-        require(!consumed_tokenId[tokenId]); // verify if the tokenId has been used in the past
-        consumed_tokenId[tokenId] = true;
+        require(!consumed_tokenId[merkle_root][tokenId]); // verify if the tokenId has been used in the past
+        consumed_tokenId[merkle_root][tokenId] = true;
     }
 }

4cc79a96878990578b33f18d820d587050e0f67a
Nishant Aklecha <nishant.aklecha@gmail.com>
2022-05-18 09:31:04 +0530

    Update v2.0 contract

--- Algoz.sol
+++ Algoz.sol
@@ -1,64 +1,32 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
 contract Algoz {
-    mapping(address => uint256) public captcha_count;
-    mapping(address => bool) public authority_wallet;
-    bytes32[] public captcha_hash_store;
+    mapping(bytes32 => bool) public consumed_tokenId;
+    address public owner;
+    bytes32 public merkle_root;
 
-    constructor() { 
-        authority_wallet[msg.sender] = true;
+    constructor(bytes32 initial_merkle_root) { 
+        owner = msg.sender;
+        merkle_root = initial_merkle_root;
     }
 
-    function check_captcha(string memory guess) public {
-        assert(get_current_hash(tx.origin) == get_hash(guess));
-        captcha_count[tx.origin] += 1;
+    function update_ownership(address new_owner) public {
+        require(msg.sender == owner); // verify contract ownership
+        owner = new_owner;
     }
 
-    function skip_captcha() public {
-        captcha_count[msg.sender] += 1;
+    function update_merkle_root(bytes32 new_merkle_root) public {
+        require(msg.sender == owner); // verify contract ownership
+        merkle_root = new_merkle_root;
     }
 
-    function add_hash(bytes32 hash) public {
-        assert(authority_wallet[msg.sender]);
-        captcha_hash_store.push(hash);
-    }
-
-    function add_hash_list(bytes32[] calldata hash_list) public {
-        assert(authority_wallet[msg.sender]);
-        for(uint256 i=0; i<hash_list.length; i++) {
-            captcha_hash_store.push(hash_list[i]);
-        }
-    }
-
-    function update_hash(uint256 index, bytes32 hash) public {
-        assert(authority_wallet[msg.sender] && index<get_length());
-        captcha_hash_store[index] = hash;
-    }
-
-    function add_authority(address new_authority_wallet) public {
-        assert(authority_wallet[msg.sender]);
-        authority_wallet[new_authority_wallet] = true;
-    }
-
-    function remove_authority() public {
-        assert(authority_wallet[msg.sender]);
-        authority_wallet[msg.sender] = false;
-    }
-
-    function get_length() public view returns(uint256) {
-        return captcha_hash_store.length;
-    }
-
-    function get_current(address wallet) public view returns(uint256) {
-        return uint256(uint256(uint160(wallet))+captcha_count[wallet])%get_length();
-    }
-
-    function get_current_hash(address wallet) public view returns(bytes32) {
-        return captcha_hash_store[get_current(wallet)];
-    }
-
-    function get_hash(string memory guess) public pure returns(bytes32) {
-        return sha256(bytes(guess));
+    // function used to validate if a captcha is valid
+    function validate_captcha(bytes32 tokenId, bytes32[] calldata proof) public {
+        require(MerkleProof.verify(proof, merkle_root, tokenId)); // verify merkle proof of tokenId
+        require(!consumed_tokenId[tokenId]); // verify if the tokenId has been used in the past
+        consumed_tokenId[tokenId] = true;
     }
 }

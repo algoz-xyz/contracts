8ac6d9513cc890e768d6df809da1b2093f4dfd56
Nishant Aklecha <nishant.aklecha@gmail.com>
2022-06-20 00:34:38 +0530

    Update Algoz contract

--- contracts/Algoz.sol
+++ contracts/Algoz.sol
@@ -1,30 +1,36 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.4;
 
-import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+import "openzeppelin-solidity/contracts/utils/cryptography/MerkleProof.sol";
 
 contract Algoz {
     mapping(bytes32 => mapping(bytes32 => bool)) public consumed_tokenId;
     address public owner;
     bytes32 public merkle_root;
 
-    constructor(bytes32 initial_merkle_root) { 
+    modifier restricted() {
+        require(
+            msg.sender == owner,
+            "This function is restricted to the contract's owner"
+        );
+        _;
+    }
+
+    constructor(bytes32 initial_merkle_root) {
         owner = msg.sender;
         merkle_root = initial_merkle_root;
     }
 
-    function update_ownership(address new_owner) public {
-        require(msg.sender == owner); // verify contract ownership
+    function update_ownership(address new_owner) public restricted {
         owner = new_owner;
     }
 
-    function update_merkle_root(bytes32 new_merkle_root) public {
-        require(msg.sender == owner); // verify contract ownership
+    function update_merkle_root(bytes32 new_merkle_root) public restricted {
         merkle_root = new_merkle_root;
     }
 
     // function used to validate if a captcha is valid
-    function validate_captcha(bytes32 tokenId, bytes32[] calldata proof) public {
+    function validate_token(bytes32 tokenId, bytes32[] calldata proof) public {
         require(MerkleProof.verify(proof, merkle_root, tokenId)); // verify merkle proof of tokenId
         require(!consumed_tokenId[merkle_root][tokenId]); // verify if the tokenId has been used in the past
         consumed_tokenId[merkle_root][tokenId] = true;
---
+++ migrations/1_miragtions.js
@@ -0,0 +1,5 @@
+const Algoz = artifacts.require("Algoz");
+
+module.exports = function (deployer) {
+  deployer.deploy(Algoz, "0x3454d3ce8d3cd97e4d0f42e72f9e277881d9759bebe0d86b0a005f7c7cfc72a1");
+};

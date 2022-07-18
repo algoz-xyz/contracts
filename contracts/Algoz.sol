ec1e3b4ba7fce20a0724107613d80f71f9097596
Nishant Aklecha <nishant.aklecha@gmail.com>
2022-07-19 00:49:12 +0530

    Update to signed tokenIds

--- contracts/Algoz.sol
+++ contracts/Algoz.sol
@@ -1,42 +1,48 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
-import "openzeppelin-solidity/contracts/utils/cryptography/MerkleProof.sol";
+import "openzeppelin-solidity/contracts/utils/cryptography/ECDSA.sol";
 
 contract Algoz {
-    mapping(bytes32 => mapping(bytes32 => bool)) public consumed_tokenId;
-    address public owner;
-    bytes32 public merkle_root;
-    uint256 public merkle_proof_length;
+    using ECDSA for bytes32;
+
+    mapping(bytes32 => bool) public consumed_tokenId;
+    address public tokenId_verifier_update_authority;
+    address public tokenId_verifier;
+
+    bool public verify_enabled;
 
     modifier restricted() {
         require(
-            msg.sender == owner,
+            msg.sender == tokenId_verifier_update_authority,
             "This function is restricted to the contract's owner"
         );
         _;
     }
 
-    constructor(bytes32 new_merkle_root, uint256 new_merkle_proof_length) {
-        owner = msg.sender;
-        update_merkle_root(new_merkle_root, new_merkle_proof_length);
+    constructor(address initial_tokenId_verifier) {
+        tokenId_verifier_update_authority = msg.sender;
+        tokenId_verifier = initial_tokenId_verifier;
+        verify_enabled = true;
     }
 
-    function update_ownership(address new_owner) public restricted {
-        owner = new_owner;
+    function update_tokenId_verifier_update_authority(address new_tokenId_verifier_update_authority) public restricted {
+        tokenId_verifier_update_authority = new_tokenId_verifier_update_authority;
     }
 
-    function update_merkle_root(bytes32 new_merkle_root, uint256 new_merkle_proof_length) public restricted {
-        merkle_root = new_merkle_root;
-        merkle_proof_length = new_merkle_proof_length;
+    function update_tokenId_verifier(address new_tokenId_verifier) public restricted {
+        tokenId_verifier = new_tokenId_verifier;
     }
 
-    // function used to validate if a captcha is valid
-    function validate_token(bytes32 tokenId, bytes32[] calldata proof) public {
-        require(MerkleProof.verify(proof, merkle_root, tokenId)); // verify merkle proof of tokenId
-        require(!consumed_tokenId[merkle_root][tokenId]); // verify if the tokenId has been used in the past
-        require(merkle_proof_length==proof.length); // validate merkle_proof_length
-        consumed_tokenId[merkle_root][tokenId] = true;
-        consumed_tokenId[merkle_root][proof[0]] = true;
+    function toggle_verify_enabled() public restricted {
+        verify_enabled = !verify_enabled;
     }
+
+    function validate_token(bytes32 tokenId, bytes calldata tokenId_proof) public {
+        if(!verify_enabled) return; // skip verification if verify_enabled is false
+        require(!consumed_tokenId[tokenId]); // verify if the tokenId has been used in the past
+        require(tokenId.recover(tokenId_proof) == tokenId_verifier); // verify if the tokenId has been used in the past
+        consumed_tokenId[tokenId] = true;
+    }
+    
 }
--- migrations/1_miragtions.js
+++ migrations/1_miragtions.js
@@ -1,5 +1,5 @@
 const Algoz = artifacts.require("Algoz");
 
 module.exports = function (deployer) {
-  deployer.deploy(Algoz, "0x3454d3ce8d3cd97e4d0f42e72f9e277881d9759bebe0d86b0a005f7c7cfc72a1", 20);
+  deployer.deploy(Algoz, "0x4F8Aa64CB65C7f7d2dB66EB7B49C76De870B243E");
 };

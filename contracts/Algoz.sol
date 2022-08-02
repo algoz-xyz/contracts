f74708e48f79b757992fad170acff6bff6360dca
Nishant Aklecha <nishant.aklecha@gmail.com>
2022-08-03 00:37:33 +0530

    Update Algoz contract

--- contracts/Algoz.sol
+++ contracts/Algoz.sol
@@ -1,48 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
-import "openzeppelin-solidity/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract Algoz {
     using ECDSA for bytes32;
+    using ECDSA for bytes;
 
-    mapping(bytes32 => bool) public consumed_tokenId;
-    address public tokenId_verifier_update_authority;
-    address public tokenId_verifier;
-
+    mapping(bytes32 => bool) public consumed_token;
+    address public token_verifier;
     bool public verify_enabled;
+    uint public proof_expiry_size;
 
-    modifier restricted() {
-        require(
-            msg.sender == tokenId_verifier_update_authority,
-            "This function is restricted to the contract's owner"
-        );
-        _;
+    constructor(address _token_verifier, bool _verify_enabled, uint _proof_expiry_size) {
+        require(_proof_expiry_size>0, "proof_expiry_size must be greater than 0");
+        token_verifier = _token_verifier;
+        verify_enabled = _verify_enabled; // should be true if the contract wants to use Algoz
+        proof_expiry_size = _proof_expiry_size; // ideally keep this as 3
     }
 
-    constructor(address initial_tokenId_verifier) {
-        tokenId_verifier_update_authority = msg.sender;
-        tokenId_verifier = initial_tokenId_verifier;
-        verify_enabled = true;
-    }
-
-    function update_tokenId_verifier_update_authority(address new_tokenId_verifier_update_authority) public restricted {
-        tokenId_verifier_update_authority = new_tokenId_verifier_update_authority;
-    }
-
-    function update_tokenId_verifier(address new_tokenId_verifier) public restricted {
-        tokenId_verifier = new_tokenId_verifier;
-    }
-
-    function toggle_verify_enabled() public restricted {
-        verify_enabled = !verify_enabled;
-    }
-
-    function validate_token(bytes32 tokenId, bytes calldata tokenId_proof) public {
+    function validate_token(bytes32 expiry_token, bytes32 auth_token, bytes calldata signature_token) public {
         if(!verify_enabled) return; // skip verification if verify_enabled is false
-        require(!consumed_tokenId[tokenId]); // verify if the tokenId has been used in the past
-        require(tokenId.recover(tokenId_proof) == tokenId_verifier); // verify if the tokenId has been used in the past
-        consumed_tokenId[tokenId] = true;
+        require(!consumed_token[auth_token]); // verify if the token has been used in the past
+        require(SafeMath.add(uint256(expiry_token), proof_expiry_size) <= block.number); // expire this proof if the current blocknumber > the expiry blocknumber
+        require(abi.encodePacked(expiry_token, auth_token).toEthSignedMessageHash().recover(signature_token) == token_verifier); // verify if the token has been used in the past
+        consumed_token[auth_token] = true;
     }
-    
 }
--- migrations/1_miragtions.js
+++
@@ -1,5 +0,0 @@
-const Algoz = artifacts.require("Algoz");
-
-module.exports = function (deployer) {
-  deployer.deploy(Algoz, "0x4F8Aa64CB65C7f7d2dB66EB7B49C76De870B243E");
-};

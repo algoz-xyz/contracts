b0518ea80a6ca540d992748b4dfecbfc48d269d5
Nishant Aklecha <nishant.aklecha@gmail.com>
2022-08-03 18:24:10 +0530

    Fix contract errors

--- contracts/Algoz.sol
+++ contracts/Algoz.sol
@@ -11,20 +11,20 @@ contract Algoz {
     mapping(bytes32 => bool) public consumed_token;
     address public token_verifier;
     bool public verify_enabled;
-    uint public proof_expiry_length;
+    uint public proof_ttl;
 
-    constructor(address _token_verifier, bool _verify_enabled, uint _proof_expiry_length) {
-        require(_proof_expiry_length>0, "proof_expiry_length must be greater than 0");
+    constructor(address _token_verifier, bool _verify_enabled, uint _proof_ttl) {
+        require(_proof_ttl>0, "AlgozInvalidTokenTTL");
         token_verifier = _token_verifier;
         verify_enabled = _verify_enabled; // should be true if the contract wants to use Algoz
-        proof_expiry_length = _proof_expiry_length; // ideally set this value to 3
+        proof_ttl = _proof_ttl; // ideally set this value to 3
     }
 
     function validate_token(bytes32 expiry_token, bytes32 auth_token, bytes calldata signature_token) public {
         if(!verify_enabled) return; // skip verification if verify_enabled is false
-        require(!consumed_token[auth_token]); // verify if the token has been used in the past
-        require(SafeMath.add(uint256(expiry_token), proof_expiry_length) <= block.number); // expire this proof if the current blocknumber > the expiry blocknumber
-        require(abi.encodePacked(expiry_token, auth_token).toEthSignedMessageHash().recover(signature_token) == token_verifier); // verify if the token has been used in the past
+        require(!consumed_token[auth_token], "AlgozConsumedTokenError"); // verify if the token has been used in the past
+        require(SafeMath.add(uint256(expiry_token), proof_ttl) >= block.number, "AlgozTokenExpiredError"); // expire this proof if the current blocknumber > the expiry blocknumber
+        require(abi.encodePacked(expiry_token, auth_token).toEthSignedMessageHash().recover(signature_token) == token_verifier, "AlgozSignatureError"); // verify if the token has been used in the past
         consumed_token[auth_token] = true;
     }
 }
